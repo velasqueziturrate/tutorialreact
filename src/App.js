14ecb2a0c44fb018de897d6809dd0b97c6190a89
Daniel Velásquez Iturrate <velasqueziturrate@gmail.com>
2019-09-04 12:37:40 -0400

    Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,84 @@
 import React from 'react';
 import './App.css';
 
+//Funcion
+/* function Helloworld(props) {
+  // console.log(props);
+  return (
+    <div id="hello">
+      <h3>{props.subtitle}</h3>
+      {props.mytext}
+    </div>
+  );
+} */
+
+//Clase Equivalente
+class Helloworld extends React.Component {
+  //Es una propiedad
+  state = {
+    //Dato del Componente, llamado show
+    show: true
+  }
+
+  //Colocarlo como funcion flecha soluciona el problema del alcance y ahorra el bind(this)
+  toggleShow = () => {
+    /* Cambia el estado de un booleano */
+    this.setState({ show: !this.state.show })
+  }
+
+  render() {
+    if (this.state.show) {
+      return (
+        <div id="hello">
+          <h3>{props.subtitle}</h3>
+          {props.mytext}
+          <button onClick={this.toggleShow.bind(this)}>Toggle Show</button>
+        </div>
+      );
+    } else {
+      return <h1>No hay elementos
+      <button onClick={this.toggleShow}>Toggle Show</button>
+      </h1>
+    }
+  }
+}
+
+//Formas Equivalentes de Hacer el Componente App 
+
+//Constante Flecha
+/* 
+  const App = () =>     
+    <div>This is my component: 
+    <Helloworld mytext="Hola Dani" subtitle="El Mejor"/> 
+    <Helloworld mytext="Hello Dani" subtitle="The Best"/> 
+    <Helloworld mytext="Ciao Dani" subtitle="Il Migliore"/>
+    </div> 
+*/
+
+//Clase
+/* 
+  class App extends React.Component {
+
+  render() {
+    return (
+      <div>This is my component:
+        <Helloworld mytext="Hola Dani" subtitle="El Mejor" />
+        <Helloworld mytext="Hello Dani" subtitle="The Best" />
+        <Helloworld mytext="Ciao Dani" subtitle="Il Migliore" />
+      </div>
+    );
+  }
+} 
+*/
+
+//Funcion
 function App() {
   return (
-    <div>Hello World!</div>
+    <div>This is my component:
+    <Helloworld mytext="Hola Dani" subtitle="El Mejor" />
+      <Helloworld mytext="Hello Dani" subtitle="The Best" />
+      <Helloworld mytext="Ciao Dani" subtitle="Il Migliore" />
+    </div>
   );
 }
 
